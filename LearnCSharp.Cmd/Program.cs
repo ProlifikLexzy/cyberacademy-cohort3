b6dc2778e4d36448a0a0778ddea029cace9f0132
Prolifik Lexzy <pro_lifik@live.com>
2022-03-31 12:02:00 +0200

    Trim down the array code

--- LearnCSharp.Cmd/Program.cs
+++ LearnCSharp.Cmd/Program.cs
@@ -7,14 +7,9 @@ namespace LearnCSharp.Cmd
         static void Main()
         {
             var anything = new int[4]{9, 3, 88, 45 };
-
-            int s = anything[3] + 1;
-            anything[3] =s;
-            anything[2] = anything[0];
-
+           
             Console.WriteLine(anything[2]);
 
-            bool[] bidemi = new bool[3] { true, true, false };
         }
     }
 }
