67aed9f3c31058a8fbd31dddbe6056d18529f2a0
Prolifik Lexzy <pro_lifik@live.com>
2022-04-11 15:30:01 +0200

    Class work answer

--- LearnCSharp.Cmd/Program.cs
+++ LearnCSharp.Cmd/Program.cs
@@ -6,34 +6,45 @@ namespace LearnCSharp.Cmd
     {
         static void Main()
         {
+            ShoppingBag<Bag> shoppingBag = new ShoppingBag<Bag>();
+            shoppingBag.AddItem(new Bag() { ProductName = "Tomato Paste", ProductCount = 67 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Corn Flakes", ProductCount = 7 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Indomie", ProductCount = 67 });
+            shoppingBag.AddItem(new Bag() { ProductName = "Hypo", ProductCount = 67 });
 
+            var counter = 0;
+            while(counter < 4)
+            {
+                Console.WriteLine(shoppingBag.GetItem(counter));
+                counter++;
+            }
         }
     }
 
-    public class IShape
-    { }
-
-    public class Square: IShape
-    { 
-        public Square()
+    public class ShoppingBag<T> where T : Bag
+    {
+        private T[] _items = new T[10];
+        private int _index = 0;
+        public void AddItem(T item)
         {
+            _items[_index++] = item;
+        }
+
+        public T GetItem(int index)
+        {
+            return _items[index];
         }
     }
-    public class Circle
+
+    public class Bag
     {
-        public Circle()
-        { }
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{ProductName} - {ProductCount}";
+        }
     }
-    
-    public class Shape<U, T> where U: IShape, new() where T : struct
-    {
-
-    }
-
-    public class Triangle: Shape<Square, bool>
-    {
-
-    }
-
-
 }
