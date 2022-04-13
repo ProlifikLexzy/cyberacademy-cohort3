78ed22e61d77643215afe8d0be856226bb367754
Prolifik Lexzy <pro_lifik@live.com>
2022-04-13 15:30:38 +0200

    Func and Action

--- LearnCSharp.Cmd/Program.cs
+++ LearnCSharp.Cmd/Program.cs
@@ -7,31 +7,28 @@ namespace LearnCSharp.Cmd
     {
         static void Main()
         {
-            var delegateArray = new ArithmeticOperationDelegate[3];
-          
-            for (int i = 0; i < delegateArray.Length; i++)
-            {
-                switch (i)
-                {
-                    case 0:
-                        delegateArray[i] = ArithmeticOperation.Add;
-                        break;
-                    case 1:
-                        delegateArray[i] = ArithmeticOperation.Minus;
-                        break;
-                    case 2:
-                        delegateArray[i] = ArithmeticOperation.Multiply;
-                        break;
-                }
+            Action<int, int, int> action = ArithmeticOperation.Method;
+            Func<int, bool, string> func = (i, b) => 
+            { 
+                return ""; 
+            };
+           var c =  func(1, true);
 
-                var del = delegateArray[i];
-                Console.WriteLine(del(8, 9));
-            }
+            var func2 = new Func<int, bool, string>[3];
+            func2[0] = ArithmeticOperation.Method2;
         }
     }
 
     public class ArithmeticOperation
     {
+        public static string Method2( int i, bool b)
+        {
+            return "";
+        }
+        public static void Method(int i, int j, int k)
+        {
+
+        }
         public static int Add(int i, int j)
         {
             return i + j;
