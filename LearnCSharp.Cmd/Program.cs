636373db3c1f67a605dc8c9fb978cb9d06e5211b
Prolifik Lexzy <pro_lifik@live.com>
2022-04-13 15:10:22 +0200

    class work done

--- LearnCSharp.Cmd/Program.cs
+++ LearnCSharp.Cmd/Program.cs
@@ -2,83 +2,50 @@
 
 namespace LearnCSharp.Cmd
 {
-    public delegate W Arithmetic<W, Y, Z>(Y first, Z second);
-    public delegate void ArithmeticDelegate();
-
-    public delegate Shape DrawDelegate();
-    public delegate void ShapeDelegate(int i);
-
-    public class Shape
-    {
-
-    }
-
-    public class Triangle : Shape
-    {
-
-    }
+    public delegate int ArithmeticOperationDelegate(int i, int j);
     public class Program
     {
-
-        static Triangle DrawTriangle()
-        {
-            return new Triangle();
-        }
-
         static void Main()
         {
-            ShapeDelegate shapeDelegate = i =>
+            var delegateArray = new ArithmeticOperationDelegate[3];
+          
+            for (int i = 0; i < delegateArray.Length; i++)
             {
-                Console.WriteLine(i);
-            };
+                switch (i)
+                {
+                    case 0:
+                        delegateArray[i] = ArithmeticOperation.Add;
+                        break;
+                    case 1:
+                        delegateArray[i] = ArithmeticOperation.Minus;
+                        break;
+                    case 2:
+                        delegateArray[i] = ArithmeticOperation.Multiply;
+                        break;
+                }
 
-            ArithmeticDelegate arithmeticDelegate = () =>
-            {
-            };
-
-            DrawDelegate drawDelegate = DrawTriangle;
-
-            Arithmetic<int, int, int> del = Add;
-            del += Minus;
-
-            //Annonymous delegate method
-            del += delegate (int first, int second)
-            {
-                return first + second;
-            };
-
-            del += delegate (int first, int second)
-            {
-                return first + second;
-            };
-
-            //Lambda Expression
-            del += (first, second) =>
-            {
-                return second + first;
-            };
-
-            del += (first, second) => second + first;
-
-
-            var result = del(39, 8);
-            Console.WriteLine(result);
-
-            //Console.WriteLine("+++++++++++++++++");
-            //del -= MyMethod2;
-            //del(80);
-
+                var del = delegateArray[i];
+                Console.WriteLine(del(8, 9));
+            }
         }
+    }
 
-        static int Add(int a, int b)
+    public class ArithmeticOperation
+    {
+        public static int Add(int i, int j)
         {
-            return a + b;
+            return i + j;
         }
 
-        static int Minus(int x, int y)
+        public static int Minus(int i, int j)
         {
-            return x - y;
+            return i - j;
         }
+        public static int Multiply(int i, int j)
+        {
+            return i * j;
+        }
+
 
     }
 }
