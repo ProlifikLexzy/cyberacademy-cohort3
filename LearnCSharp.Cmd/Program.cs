4b84bb65b9ceff4f5662e39d837fdd5952024f8c
Prolifik Lexzy <pro_lifik@live.com>
2022-04-21 10:21:16 +0100

    Learning IEnumerable

--- LearnCSharp.Cmd/Program.cs
+++ LearnCSharp.Cmd/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using LearnCSharp.Library;
 
 namespace LearnCSharp.Cmd
@@ -7,6 +9,40 @@ namespace LearnCSharp.Cmd
     {
         public static void Main()
         {
+            var words = "Hello World";
+
+            var employees = new Employee[]
+            {
+                new Employee(){ FullName = "Warren Buffet"},
+                new Employee(){ FullName = "Steve Jobs"},
+                new Employee{ FullName ="Bill Gates"}
+            };
+
+
+
+            var employees2 = new Employee[]
+            {
+                new Employee(){ FullName = "Jim Ovia"},
+                new Employee(){ FullName = "Aliko Dangote"},
+                new Employee{ FullName ="Mike Adenuga"}
+            };
+
+            var staffs = new Staffs(employees);
+            var staffs2 = new Staffs(employees2);
+
+
+            foreach (var item in staffs)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("=================");
+            foreach (var item in staffs)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
         }
     }
 }
